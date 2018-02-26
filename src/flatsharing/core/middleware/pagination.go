cea7b25ad21086b09a053210bb1c5c2ac57c354e
Alexandre MARRE <me@itsalex.fr>
2018-02-26 22:04:27 +0100

    Update pagination with echo binding

--- src/flatsharing/core/middleware/pagination.go
+++ src/flatsharing/core/middleware/pagination.go
@@ -1,36 +1,27 @@
 package middleware
 
 import (
-	"strconv"
-
 	"github.com/labstack/echo"
 )
 
 // Pagination struct to handle pagination
 type Pagination struct {
-	Page  int
-	Limit int
+	Page  int `query:"page"`
+	Limit int `query:"limit"`
 }
 
 // HydratePagination hydrate echo context with pagination
 func HydratePagination(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		pagination := Pagination{
-			Page:  1,
-			Limit: 50,
-		}
+		var pagination Pagination
 
-		if val := c.QueryParam("page"); val != "" {
-			page, _ := strconv.Atoi(val)
-			if page > 0 {
-				pagination.Page = page
-			}
+		c.Bind(&pagination)
+
+		if pagination.Page <= 0 {
+			pagination.Page = 1
 		}
-		if val := c.QueryParam("limit"); val != "" {
-			limit, _ := strconv.Atoi(val)
-			if limit > 0 {
-				pagination.Limit = limit
-			}
+		if pagination.Limit <= 0 {
+			pagination.Limit = 50
 		}
 
 		c.Set("pagination", pagination)
