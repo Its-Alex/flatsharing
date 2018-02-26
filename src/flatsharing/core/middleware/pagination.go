53ce1c7b4fef488963fc9afbbdc5dee17f2a85c0
Alexandre MARRE <me@itsalex.fr>
2018-02-27 00:10:22 +0100

    Rollback to reuse bind... Echo read stream and can't read it again

--- src/flatsharing/core/middleware/pagination.go
+++ src/flatsharing/core/middleware/pagination.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"strconv"
+
 	"github.com/labstack/echo"
 )
 
@@ -13,15 +15,22 @@ type Pagination struct {
 // HydratePagination hydrate echo context with pagination
 func HydratePagination(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		var pagination Pagination
-
-		_ = c.Bind(&pagination)
-
-		if pagination.Page <= 0 {
-			pagination.Page = 1
+		pagination := Pagination{
+			Page:  1,
+			Limit: 50,
 		}
-		if pagination.Limit <= 0 {
-			pagination.Limit = 50
+
+		if val := c.QueryParam("page"); val != "" {
+			page, _ := strconv.Atoi(val)
+			if page > 0 {
+				pagination.Page = page
+			}
+		}
+		if val := c.QueryParam("limit"); val != "" {
+			limit, _ := strconv.Atoi(val)
+			if limit > 0 {
+				pagination.Limit = limit
+			}
 		}
 
 		c.Set("pagination", pagination)
