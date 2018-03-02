7a56e21fd152df7e215baad163ad44e611f15dab
Alexandre MARRE <me@itsalex.fr>
2018-03-02 23:36:00 +0100

    Add first migration schema

--- migration/1_add_table.up.sql
+++ migration/1_add_table.up.sql
@@ -1,19 +1,48 @@
 CREATE TABLE users(
-   id char(26) NOT NULL unique,
-   mail varchar(128) NOT NULL unique,
-   login varchar(36) NOT NULL unique,
-   username varchar(36),
-   firstname varchar(36),
-   lastname varchar(36),
+   id char(26) NOT NULL,
+   mail varchar(128) NOT NULL UNIQUE,
+   login varchar(64) NOT NULL UNIQUE,
+   username varchar(64),
+   firstname varchar(64),
+   lastname varchar(64),
    password bytea,
    role int NOT NULL,
-   date timestamptz NOT NULL DEFAULT NOW(),
-   PRIMARY KEY(id)
+   created_at timestamptz NOT NULL DEFAULT NOW(),
+   PRIMARY KEY (id)
 );
 
 CREATE TABLE tokens(
-    id char(26) NOT NULL unique,
-    token char(128) NOT NULL unique,
-    date timestamptz NOT NULL DEFAULT NOW(),
-    PRIMARY KEY(id)
+    id char(26) NOT NULL,
+    fk_user_id char(26) NOT NULL,
+    token char(128) NOT NULL UNIQUE,
+    created_at timestamptz NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (id),
+    CONSTRAINT fk_user_id FOREIGN KEY (fk_user_id) REFERENCES users(id)
+);
+
+CREATE TABLE flatsharings(
+    id char(26) NOT NULL,
+    name varchar(64) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE shops(
+    id char(26) NOT NULL,
+    name varchar(64) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE purchases(
+    id char(26) NOT NULL,
+    fk_user_id char(26) NOT NULL,
+    fk_buyer_id char(26) NOT NULL,
+    fk_shop_id char(26) NOT NULL,
+    price numeric(8, 2) NOT NULL,
+    description text NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (id),
+    CONSTRAINT fk_user_id FOREIGN KEY (fk_user_id) REFERENCES users(id),
+    CONSTRAINT fk_buyer_id FOREIGN KEY (fk_buyer_id) REFERENCES users(id),
+    CONSTRAINT fk_shop_id FOREIGN KEY (fk_shop_id) REFERENCES shops(id),
+    CONSTRAINT positive_price CHECK (price > 0)
 );
